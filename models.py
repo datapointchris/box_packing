2fdef6171c6910db166154a6fc5f7134dee9a844
Chris Birch <chrisbirch@live.com>
2021-10-20 12:27:23 -0600

    update: Base tables configured

--- models.py
+++ models.py
@@ -1,32 +1,39 @@
-from sqlalchemy import create_engine
-from sqlalchemy import MetaData
-from sqlalchemy import Table, Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy.orm import declarative_base, relationship
 
-engine = create_engine('sqlite+pysqlite:///:memory:', echo=True, future=True)
-metadata = MetaData()
+Base = declarative_base()
 
-boxes_table = Table(
-    'boxes',
-    metadata,
-    Column('id', Integer, primary_key=True),
-    Column('name', String(30)),
-    Column('size', String(10)),
-    Column('length', Integer),
-    Column('width', Integer),
-    Column('height', Integer),
-    Column('weight_class', Integer),
-    Column('fragile', Boolean),
-    Column('liquid', Boolean),
-    Column('valuable', Boolean),
-    Column('weather_resistant', Boolean)
-)
 
-items_table = Table(
-    'items',
-    metadata,
-    Column('id', Integer, primary_key=True),
-    Column('box_id', ForeignKey('boxes.id'), nullable=False),
-    Column('name', String),
-    Column('fragile', Boolean)
-)
+class Box(Base):
+    __tablename__ = 'boxes'
 
+    id = Column('id', Integer, primary_key=True)
+    name = Column('name', String(30))
+    size = Column('size', String(10))
+    length = Column('length', Integer)
+    width = Column('width', Integer)
+    height = Column('height', Integer)
+    weight_class = Column('weight_class', Integer)
+    fragile = Column('fragile', Boolean, default=False)
+    liquid = Column('liquid', Boolean, default=False)
+    valuable = Column('valuable', Boolean, default=False)
+    weather_resistant = Column('weather_resistant', Boolean, default=False)
+
+    items = relationship('Items', back_populates='boxes')
+
+    def __repr__(self):
+        return f'Box(id={self.id!r}, name={self.name!r}, size={self.size!r}'
+
+
+class Item(Base):
+    __tablename__ = 'items'
+
+    id = Column('id', Integer, primary_key=True)
+    box_id = Column('box_id', ForeignKey('boxes.id'), nullable=False)
+    name = Column('name', String)
+    fragile = Column('fragile', Boolean, default=False)
+
+    boxes = relationship('Boxes', back_populates='items')
+
+    def __repr__(self):
+        return f'Item(id={self.id!r}, name={self.name!r}, box_id={self.box_id!r}'
