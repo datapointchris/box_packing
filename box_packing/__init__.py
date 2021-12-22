8e13f4530c5c9242d4b9cfa481c1e2a1c0994f1c
Chris Birch <datapointchris@gmail.com>
2021-12-22 07:18:02 -0700

    add app context

--- box_packing/__init__.py
+++ box_packing/__init__.py
@@ -12,11 +12,11 @@ connection.row_factory = sqlite3.Row
 db = BoxManager(connection)
 
 
-with open(url_for('static', filename='sql/create_db.sql'), 'r') as f:
-    db.run_sql_script(f)
+with app.app_context():
+    with open(url_for('static', filename='sql/create_db.sql'), 'r') as f:
+        db.run_sql_script(f)
 
-# with app.app_context():
-#     db.create_db()
+
 
 
 from box_packing import routes
