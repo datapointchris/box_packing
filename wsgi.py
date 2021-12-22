e7c433f5fa589b6acf2f765d627afb9ae3fd9bca
Chris Birch <datapointchris@gmail.com>
2021-12-22 13:16:26 -0700

    change nginx

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,10 @@
 "App entry point"
 from box_packing import create_app
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = create_app()
+
+app.wsgi_app = ProxyFix(app.wsgi_app, x_prefix=1)
 
 if __name__ == '__main__':
     app.run(port=8000)
