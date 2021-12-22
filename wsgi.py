9558fef6dd804d20aa1edfd24add9454a8454a76
Chris Birch <datapointchris@gmail.com>
2021-12-22 14:14:23 -0700

    update wsgi

--- wsgi.py
+++ wsgi.py
@@ -4,7 +4,7 @@ from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = create_app()
 
-app.wsgi_app = ProxyFix(app.wsgi_app, x_prefix=1)
+app.wsgi_app = ProxyFix(app.wsgi_app, x_prefix=1, x_for=1, x_host=1, x_proto=1)
 
 if __name__ == '__main__':
     app.run(port=8000)
