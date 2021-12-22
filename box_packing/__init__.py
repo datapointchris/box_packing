01fe2c20da33b8a21f92b3410efacb2f05d0126d
Chris Birch <datapointchris@gmail.com>
2021-12-22 14:56:11 -0700

    change application root

--- box_packing/__init__.py
+++ box_packing/__init__.py
@@ -12,6 +12,7 @@ db = BoxManager(connection)
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = '1A37BbcCJh67'
+    app.config['APPLICATION_ROOT'] = '/moving'
 
     with app.app_context():
         from box_packing import routes
