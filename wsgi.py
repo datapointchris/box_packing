2b5a2ae341d96f38117c18971e002eb05346fe1c
Chris Birch <datapointchris@gmail.com>
2022-02-02 22:37:01 -0600

    Update entry point wsgi with conditionals, move deploy folder out

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,27 @@
 "App entry point"
+import platform
 from box_packing import create_app
+
+develop = False
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(port=8000)
+    if develop:
+        from livereload import Server
+
+        server = Server(app)
+        server.watch('static/')
+        if platform.system() == 'Darwin':
+            # Mac
+            server.serve(port=4000, host='0.0.0.0')
+        else:
+            # Linux Box
+            server.serve(port=8000)
+    else:
+        if platform.system() == 'Darwin':
+            # Mac
+            app.run(port=4000, host='0.0.0.0')
+        else:
+            # Linux Box
+            app.run(port='8000')
